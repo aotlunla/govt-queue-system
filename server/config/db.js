38687f8baff2996f49654ccf5d3d3e27383b5c83
Nutkit90s <aotlunla001@hotmail.co.th>
2026-06-16 17:14:04 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,11 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST || process.env.DB_HOST,
+  port: process.env.MYSQLPORT || process.env.MYSQL_PORT || process.env.DB_PORT || 3306,
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
